533e23218837ecd8bb7268ebfe0eb2eb59d11cab
Abdiel Fonseca <161096063+Abdiel-San@users.noreply.github.com>
2026-04-15 21:39:44 -0600

    Remove Delta table creation for raw Spotify tracks
    Removed the Delta table creation statement for raw Spotify tracks.

--- notebooks/bronze_ingestion.py
+++ notebooks/bronze_ingestion.py
@@ -4,7 +4,3 @@ CREATE SCHEMA IF NOT EXISTS dev_workspace;
 -- Crear el schema bronze dentro del workspace
 CREATE SCHEMA IF NOT EXISTS dev_workspace.bronze;
 
--- Crear la tabla Delta donde escribirás el streaming
-CREATE TABLE IF NOT EXISTS dev_workspace.bronze.raw_spotify_tracks
-USING DELTA
-LOCATION 's3://aws-data-lake-databricks/tables/bronze/raw_spotify_tracks';
