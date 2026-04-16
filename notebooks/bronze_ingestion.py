b50a6b1dfeb0a35e4657d4313e804beded0f4dd3
Abdiel Fonseca <161096063+Abdiel-San@users.noreply.github.com>
2026-04-15 21:00:58 -0600

    Create schemas and Delta table for Spotify tracks

--- notebooks/bronze_ingestion.py
+++ notebooks/bronze_ingestion.py
@@ -1,2 +1,10 @@
+-- Crear el schema principal 
 CREATE SCHEMA IF NOT EXISTS dev_workspace;
+
+-- Crear el schema bronze dentro del workspace
 CREATE SCHEMA IF NOT EXISTS dev_workspace.bronze;
+
+-- Crear la tabla Delta donde escribirás el streaming
+CREATE TABLE IF NOT EXISTS dev_workspace.bronze.raw_spotify_tracks
+USING DELTA
+LOCATION 's3://aws-data-lake-databricks/tables/bronze/raw_spotify_tracks';
